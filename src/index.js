d0ce3658bfa16c8a2b636642a85126350e12c69c
Miguel Boanovam <mgl.deadly@gmail.com>
2019-10-19 14:34:54 -0300

    Crud de Writer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ const Book = require('./models/book')
 
 
 const typeDefs = gql`
+    enum Genres {
+        COMEDY
+        DRAMA
+        FANTASY
+        ACTION
+        ADVENTURE
+        HORROR
+        ROMANCE
+    }
+
+
     type Writer {
         id: ID!
         firstname: String!
@@ -21,7 +32,7 @@ const typeDefs = gql`
         title: String!
         ISBN: Int!
         publicationDate: String!
-        genre: String
+        genre: Genres
         writer: Writer!
     }
 
@@ -31,14 +42,12 @@ const typeDefs = gql`
 
     type Mutation {
         createBook(data: CreateBookInput): Book
-    }
+        updateBook(id: ID! data: UpdateBookInput): Book
+        deleteBook(id: ID!): Boolean
 
-    input CreateBookInput {
-        title: String!
-        ISBN: Int!
-        publicationDate: String!
-        genre: String
-        writer: CreateWriterInput!
+        createWriter(data: CreateWriterInput): Writer
+        updateWriter(id: ID! data: UpdateWriterInput): Writer
+        deleteWriter(id: ID!): Boolean
     }
 
     input CreateWriterInput {
@@ -50,6 +59,29 @@ const typeDefs = gql`
         phone: String
     }
 
+    input UpdateWriterInput {
+        firstname: String
+        lastname: String
+        initials: String
+        birthday: String
+        gender: String
+        phone: String
+    }
+
+    input CreateBookInput {
+        title: String!
+        ISBN: Int!
+        publicationDate: String!
+        genre: Genres
+        writer: CreateWriterInput!
+    }
+
+    input UpdateBookInput {
+        title: String
+        ISBN: Int
+        publicationDate: String
+        genre: Genres
+    }
 `
 
 const resolver = {
@@ -69,7 +101,45 @@ const resolver = {
             await book.setWriter(createdWriter.get('id'))
 
             return book.reload({ include: [Writer] })
-        }
+        },
+        async updateBook(parent, body, context, info) {
+            const book = await Book.findOne({
+                where: { id: body.id }
+            })
+            if (!book) {
+                throw new Error('Livro não encontrado')
+            }
+            const updatedBook = await book.update(body.data)
+            return updatedBook
+        },
+        async deleteBook(parent, body, context, info) {
+            const book = await Book.findOne({
+                where: { id: body.id }
+            })
+            await book.destroy()
+            return true
+        },
+        async createWriter(parent, body, context, info) {
+            const writer = await Writer.create(body.data)
+            return writer.reload({ include: [Book] })
+        },
+        async updateWriter(parent, body, context, info) {
+            const writer = await Writer.findOne({
+                where: { id: body.id }
+            })
+            if (!writer) {
+                throw new Error('Autor não encontrado')
+            }
+            const updateWriter = await writer.update(body.data)
+            return updateWriter
+        },
+        async deleteWriter(parent, body, context, info) {
+            const writer = await Writer.findOne({
+                where: { id: body.id }
+            })
+            await writer.destroy()
+            return true
+        },
     }
 }
 
